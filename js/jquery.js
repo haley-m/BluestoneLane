2b6841ec3845217d0053e5531bc3023b54524d96
haley-m <124923973+haley-m@users.noreply.github.com>
2024-07-05 14:37:38 +0900

    JQuery

---
+++ js/jquery.js
@@ -0,0 +1,11 @@
+// <!-- Initialize Swiper -->
+<script>
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    // spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+</script>
